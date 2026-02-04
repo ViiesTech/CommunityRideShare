e79506111d1a53f9277bc2f9c259d1dab4226857
ViiesTech <viistech.developer@gmail.com>
2026-02-05 03:39:10 +0500

    Fix Android build configuration

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes/Routes';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { View,Text } from 'react-native';
 
 const App = () => {
   return (
     <NavigationContainer>
       <SafeAreaView style={{ flex: 1 }} >
-        <Routes />
+        {/* <Routes /> */}
+        <View><Text>hello</Text></View>
       </SafeAreaView>
     </NavigationContainer>
   );
