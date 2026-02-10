5045b2e8483d10aada39f5931ea252c1762246b2
ViiesTech <visstechapps@gmail.com>
2026-02-10 23:27:08 +0500

    fix: run the Project.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react'
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes/Routes';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View,Text } from 'react-native';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView style={{ flex: 1 }} >
-        {/* <Routes /> */}
-        <View><Text>hello</Text></View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Routes />
       </SafeAreaView>
     </NavigationContainer>
   );
